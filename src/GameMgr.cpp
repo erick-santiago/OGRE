a199d7d93f430efd98dcaa0b6f38d62b6f5f5dd4
erick-at-github <esantiago@nevada.unr.edu>
2020-07-11 16:45:35 -0700

    adding readme sources

--- src/GameMgr.cpp
+++ src/GameMgr.cpp
@@ -17,11 +17,7 @@
 #include <OgreMeshManager.h>
 
 #include <OgreEntity.h>
-//#include <OgreConfigFile.h>
-//#include "OgreRenderWindow.h"
 #include <OgreSceneManager.h>
-//#include <OgreCamera.h>
-//#include "OgreViewport.h"
 #include <iostream>
 
 #include <OgreException.h>
@@ -42,45 +38,45 @@ void GameMgr::Init()  // left off here... running the program... maybe should be
 	// setting up the Scene ***************************************
 
 	const Ogre::Real surfaceHeight = 0;
-		EntityType Destroyer = DESTROYER;
-		EntityType Carrier	 = CARRIER;
-		EntityType Speedboat = SPEEDBOAT;
-		EntityType Frigate	 = FRIGATE;
-		EntityType AlienShip = ALIENSHIP;
+	EntityType Destroyer = DESTROYER;
+	EntityType Carrier	 = CARRIER;
+	EntityType Speedboat = SPEEDBOAT;
+	EntityType Frigate	 = FRIGATE;
+	EntityType AlienShip = ALIENSHIP;
 
-	    mSceneMgr->setAmbientLight(Ogre::ColourValue(.25, .25, .25));
+	mSceneMgr->setAmbientLight(Ogre::ColourValue(.25, .25, .25));
 
-	  	// Create ground
-	    Ogre::Plane plane(Ogre::Vector3::UNIT_Y, surfaceHeight);
+	// Create ground
+	Ogre::Plane plane(Ogre::Vector3::UNIT_Y, surfaceHeight);
 
-	    Ogre::MeshManager::getSingleton().createPlane(
-	      "ground",
-	      Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
-	      plane, 10000, 10000, 20, 20, true, 1, 5, 5, Ogre::Vector3::UNIT_Z);
+	Ogre::MeshManager::getSingleton().createPlane(
+	  "ground",
+	  Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+	  plane, 10000, 10000, 20, 20, true, 1, 5, 5, Ogre::Vector3::UNIT_Z);
 
-	    Ogre::Entity* groundEntity = mSceneMgr->createEntity("ground");
-	    mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(groundEntity);
+	Ogre::Entity* groundEntity = mSceneMgr->createEntity("ground");
+	mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(groundEntity);
 
-	    groundEntity->setMaterialName("Ocean2_Cg");
-	    groundEntity->setCastShadows(false);
+	groundEntity->setMaterialName("Ocean2_Cg");
+	groundEntity->setCastShadows(false);
 
-	    // Create SkyBox
-	    mSceneMgr->setSkyBox(true, "Examples/MorningSkyBox");
+	// Create SkyBox
+	mSceneMgr->setSkyBox(true, "Examples/MorningSkyBox");
 
-	  Ogre::Light* pointLight = mSceneMgr->createLight("PointLight");
-	  pointLight->setType(Ogre::Light::LT_POINT);
-	  pointLight->setPosition(250, 150, 250);
-	  pointLight->setDiffuseColour(Ogre::ColourValue::White);
-	  pointLight->setSpecularColour(Ogre::ColourValue::White);
+	Ogre::Light* pointLight = mSceneMgr->createLight("PointLight");
+	pointLight->setType(Ogre::Light::LT_POINT);
+	pointLight->setPosition(250, 150, 250);
+	pointLight->setDiffuseColour(Ogre::ColourValue::White);
+	pointLight->setSpecularColour(Ogre::ColourValue::White);
 
-	  engine->entityMgr->ev.push_back( engine->entityMgr->createMyEntity(AlienShip, Ogre::Vector3(-100,1,300)) );
-	  engine->entityMgr->ev.push_back( engine->entityMgr->createMyEntity(Speedboat, Ogre::Vector3(-100,1,150)) );
-	  engine->entityMgr->ev.push_back( engine->entityMgr->createMyEntity(Frigate, Ogre::Vector3(-100,1,0)) );
-	  engine->entityMgr->ev.push_back( engine->entityMgr->createMyEntity(Destroyer, Ogre::Vector3(-100,1,-150)) );
-	  engine->entityMgr->ev.push_back( engine->entityMgr->createMyEntity(Carrier, Ogre::Vector3(-100,1,-300)) );
+	engine->entityMgr->ev.push_back( engine->entityMgr->createMyEntity(AlienShip, Ogre::Vector3(-100,1,300)) );
+	engine->entityMgr->ev.push_back( engine->entityMgr->createMyEntity(Speedboat, Ogre::Vector3(-100,1,150)) );
+	engine->entityMgr->ev.push_back( engine->entityMgr->createMyEntity(Frigate, Ogre::Vector3(-100,1,0)) );
+	engine->entityMgr->ev.push_back( engine->entityMgr->createMyEntity(Destroyer, Ogre::Vector3(-100,1,-150)) );
+	engine->entityMgr->ev.push_back( engine->entityMgr->createMyEntity(Carrier, Ogre::Vector3(-100,1,-300)) );
 
 
-	  engine->entityMgr->ev.at(vPos)->scnNode->showBoundingBox(true);
+	engine->entityMgr->ev.at(vPos)->scnNode->showBoundingBox(true);
 }
 
 void GameMgr::LoadLevel()
@@ -97,7 +93,4 @@ void GameMgr::Stop()
 {
 
 }
-
-
-// left off here... might remove some #includes...
 
--- src/GfxMgr.cpp
+++ src/GfxMgr.cpp
@@ -53,66 +53,65 @@ void GfxMgr::Init()
   Ogre::String name, locType;
   Ogre::ConfigFile::SectionIterator secIt = cf.getSectionIterator();
 
-    while (secIt.hasMoreElements())
+  while (secIt.hasMoreElements())
+  {
+    Ogre::ConfigFile::SettingsMultiMap* settings = secIt.getNext();
+    Ogre::ConfigFile::SettingsMultiMap::iterator it;
+
+    for (it = settings->begin(); it != settings->end(); ++it)
     {
-      Ogre::ConfigFile::SettingsMultiMap* settings = secIt.getNext();
-      Ogre::ConfigFile::SettingsMultiMap::iterator it;
+      locType = it->first;
+      name = it->second;
 
-      for (it = settings->begin(); it != settings->end(); ++it)
-      {
-        locType = it->first;
-        name = it->second;
-
-        Ogre::ResourceGroupManager::getSingleton().addResourceLocation(name, locType);
-      }
+      Ogre::ResourceGroupManager::getSingleton().addResourceLocation(name, locType);
     }
+  }
 
-    // configuring the RenderSystem ******************************
+  // configuring the RenderSystem ******************************
 
-    if (!(mRoot->restoreConfig() || mRoot->showConfigDialog()))
-    {
-    	std::cout << "\nNeed to throw Exception here and delete 'Ogre.cfg' file...";
-    }
+  if (!(mRoot->restoreConfig() || mRoot->showConfigDialog()))
+  {
+    std::cout << "\nNeed to throw Exception here and delete 'Ogre.cfg' file...";
+  }
 
-    // creating a RenderWindow ************************************
+  // creating a RenderWindow ************************************
 
-    mWindow = mRoot->initialise(true, "As4 Render Window");
+  mWindow = mRoot->initialise(true, "As4 Render Window");
 
-    // initializing resources *************************************
+  // initializing resources *************************************
 
-    Ogre::TextureManager::getSingleton().setDefaultNumMipmaps(5);
+  Ogre::TextureManager::getSingleton().setDefaultNumMipmaps(5);
 
-    Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+  Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
 
-    // creating a Scene Manager************************************
+  // creating a Scene Manager************************************
 
-        mSceneMgr = mRoot->createSceneManager(Ogre::ST_GENERIC);
+  mSceneMgr = mRoot->createSceneManager(Ogre::ST_GENERIC);
 
-    // creating the Camera ****************************************
+  // creating the Camera ****************************************
 
-    mCamera = mSceneMgr->createCamera("MainCam");
+  mCamera = mSceneMgr->createCamera("MainCam");
 
-    mCamera->setPosition(Ogre::Vector3(0, 75, 600));
-    mCamera->lookAt(Ogre::Vector3(100, 0, -200));
-    mCamera->setNearClipDistance(5);
+  mCamera->setPosition(Ogre::Vector3(0, 75, 600));
+  mCamera->lookAt(Ogre::Vector3(100, 0, -200));
+  mCamera->setNearClipDistance(5);
 
-    // A node to attach the camera to so we can move the camera node instead of the camera.
-    cameraNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
-    cameraNode->setPosition(0, 200, 500);
-    cameraNode->attachObject(mCamera);
+  // A node to attach the camera to so we can move the camera node instead of the camera.
+  cameraNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+  cameraNode->setPosition(0, 75, 150);
+  cameraNode->attachObject(mCamera);
 
-    // adding a ViewPort ******************************************
+  // adding a ViewPort ******************************************
 
-    Ogre::Viewport* vp = mWindow->addViewport(mCamera);
+  Ogre::Viewport* vp = mWindow->addViewport(mCamera);
 
-    vp->setBackgroundColour(Ogre::ColourValue(0,0,0));
+  vp->setBackgroundColour(Ogre::ColourValue(0,0,0));
 
-    mCamera->setAspectRatio(
-      Ogre::Real(vp->getActualWidth()) /
-      Ogre::Real(vp->getActualHeight()));
+  mCamera->setAspectRatio(
+    Ogre::Real(vp->getActualWidth()) /Ogre::Real(vp->getActualHeight()));
 
-    // registering our application as a FrameListener
-    //mRoot->addFrameListener(this);
+  // registering our application as a FrameListener
+  //mRoot->addFrameListener(this);
 
 
 }
@@ -128,8 +127,8 @@ void GfxMgr::Tick(float dt)
 
 	if( !mRoot->renderOneFrame() )
 	{
-		Ogre::LogManager::getSingletonPtr()->logMessage("***Test... renderOneFrame() is called and is false...  ***");
-		engine->keepRunning = false;
+      Ogre::LogManager::getSingletonPtr()->logMessage("***Test... renderOneFrame() is called and is false...  ***");
+	engine->keepRunning = false;
 	}
 }
 
